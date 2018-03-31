744a8b156e7b0d1792c325fdf6e4ffb98ac39440
Christian Römer <christian.uwe.roemer@gmail.com>
2018-03-31 20:23:12 +0200

    changed recursion functions for greatest common divisor and factorial to tailrec.

--- src/main/scala/Recursion.scala
+++ src/main/scala/Recursion.scala
@@ -1,7 +1,19 @@
+import scala.annotation.tailrec
+
 object Recursion extends App {
+  @tailrec
   def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
 
   def factorial(n: Int): Int =
     if (n == 0) 1 else n * factorial(n - 1)
+
+  def factorialTailRec(n: Int): Int = {
+    @tailrec
+    def iter(x: Int, result: Int): Int =
+      if (x == 0) result
+      else iter(x - 1, result * x)
+
+    iter(n, 1)
+  }
 }
